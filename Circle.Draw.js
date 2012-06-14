0847bfeee54ac495f26a594adbd0b4414dda7031
jacobtoye <jacob.toye@gmail.com>
2012-06-14 16:53:27 +1200

    Refactoring Rectangle and Circle classes
    Was lots of duplicate code, more OOP ftw!

--- Circle.Draw.js
+++ Circle.Draw.js
@@ -1,4 +1,4 @@
-L.Circle.Draw = L.Handler.Draw.extend({
+L.Circle.Draw = L.SimpleShape.Draw.extend({
 	options: {
 		shapeOptions: {
 			stroke: true,
@@ -12,81 +12,21 @@ L.Circle.Draw = L.Handler.Draw.extend({
 		}
 	},
 
-	addHooks: function () {
-		L.Handler.Draw.prototype.addHooks.call(this);
-		if (this._map) {
-			this._map.dragging.disable();
-			//TODO refactor: move cursor to styles
-			this._container.style.cursor = 'crosshair';
+	_initialLabelText: 'Click and drag to draw circle.',
 
-			this._updateLabelText({ text: 'Click and drag to draw circle.' });
-
-			L.DomEvent
-				.addListener(this._container, 'mousedown', this._onMouseDown, this)
-				.addListener(document, 'mousemove', this._onMouseMove, this);
+	_drawShape: function (latlng) {
+		if (!this._shape) {
+			this._shape = new L.Circle(this._startLatLng, this._startLatLng.distanceTo(latlng), this.options.shapeOptions);
+			this._map.addLayer(this._shape);
+		} else {
+			this._shape.setRadius(this._startLatLng.distanceTo(latlng));
 		}
 	},
 
-	removeHooks: function () {
-		L.Handler.Draw.prototype.removeHooks.call(this);
-		if (this._map) {
-			this._map.dragging.enable();
-			//TODO refactor: move cursor to styles
-			this._container.style.cursor = '';
-
-			L.DomEvent
-				.removeListener(this._container, 'mousedown', this._onMouseDown)
-				.removeListener(document, 'mousemove', this._onMouseMove)
-				.removeListener(document, 'mouseup', this._onMouseUp);
-
-			// If the circle element doesn't exist they must not have moved the mouse, so don't need to destroy/return
-			if (this._circ) {
-				this._map.removeLayer(this._circ);
-				delete this._circ;
-			}
-		}
-		this._isDrawing = false;
-	},
-
-	_onMouseDown: function (e) {
-		this._isDrawing = true;
-		
-		this._updateLabelText({ text: 'Release mouse to finish drawing.' });
-
-		this._startLatLng = this._map.mouseEventToLatLng(e);
-
-		this._startLayerPoint = this._map.mouseEventToLayerPoint(e);
-
-		L.DomEvent
-			.addListener(document, 'mouseup', this._onMouseUp, this)
-			.preventDefault(e);
-	},
-
-	_onMouseMove: function (e) {
-		var layerPoint = this._map.mouseEventToLayerPoint(e),
-			latlng = this._map.mouseEventToLatLng(e);
-
-		this._updateLabelPosition(layerPoint);
-
-		if (this._isDrawing) {
-			this._updateLabelPosition(layerPoint);
-			if (!this._circ) {
-				this._circ = new L.Circle(this._startLatLng, this._startLatLng.distanceTo(latlng), this.options.shapeOptions);
-				this._map.addLayer(this._circ);
-			} else {
-				this._circ.setRadius(this._startLatLng.distanceTo(latlng));
-			}
-		}
-	},
-
-	_onMouseUp: function (e) {
-		this._endLatLng = this._map.mouseEventToLatLng(e);
-		
+	_fireCreatedEvent: function () {
 		this._map.fire(
 			'draw:circle-created',
 			{ circ: new L.Circle(this._startLatLng, this._startLatLng.distanceTo(this._endLatLng), this.options.shapeOptions) }
 		);
-		
-		this.disable();
 	}
 });
---
+++ SimpleShape.Draw.js
@@ -0,0 +1,84 @@
+L.SimpleShape = {};
+
+L.SimpleShape.Draw = L.Handler.Draw.extend({
+	options: {
+		shapeOptions: {
+			stroke: true,
+			color: '#f06eaa',
+			weight: 4,
+			opacity: 0.5,
+			fill: true,
+			fillColor: null, //same as color by default
+			fillOpacity: 0.2,
+			clickable: true
+		}
+	},
+
+	addHooks: function () {
+		L.Handler.Draw.prototype.addHooks.call(this);
+		if (this._map) {
+			this._map.dragging.disable();
+			//TODO refactor: move cursor to styles
+			this._container.style.cursor = 'crosshair';
+
+			this._updateLabelText({ text: this._initialLabelText });
+
+			L.DomEvent
+				.addListener(this._container, 'mousedown', this._onMouseDown, this)
+				.addListener(document, 'mousemove', this._onMouseMove, this);
+		}
+	},
+
+	removeHooks: function () {
+		L.Handler.Draw.prototype.removeHooks.call(this);
+		if (this._map) {
+			this._map.dragging.enable();
+			//TODO refactor: move cursor to styles
+			this._container.style.cursor = '';
+
+			L.DomEvent
+				.removeListener(this._container, 'mousedown', this._onMouseDown)
+				.removeListener(document, 'mousemove', this._onMouseMove)
+				.removeListener(document, 'mouseup', this._onMouseUp);
+
+			// If the box element doesn't exist they must not have moved the mouse, so don't need to destroy/return
+			if (this._shape) {
+				this._map.removeLayer(this._shape);
+				delete this._shape;
+			}
+		}
+		this._isDrawing = false;
+	},
+
+	_onMouseDown: function (e) {
+		this._isDrawing = true;
+		
+		this._updateLabelText({ text: 'Release mouse to finish drawing.' });
+
+		this._startLatLng = this._map.mouseEventToLatLng(e);
+
+		L.DomEvent
+			.addListener(document, 'mouseup', this._onMouseUp, this)
+			.preventDefault(e);
+	},
+
+	_onMouseMove: function (e) {
+		var layerPoint = this._map.mouseEventToLayerPoint(e),
+			latlng = this._map.mouseEventToLatLng(e);
+
+		this._updateLabelPosition(layerPoint);
+
+		if (this._isDrawing) {
+			this._updateLabelPosition(layerPoint);
+			this._drawShape(latlng);
+		}
+	},
+
+	_onMouseUp: function (e) {
+		this._endLatLng = this._map.mouseEventToLatLng(e);
+
+		this._fireCreatedEvent();
+		
+		this.disable();
+	}
+});
