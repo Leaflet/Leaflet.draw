38ea56fe4be929b498478e21931220ba485dea5f
jacobtoye <jacob.toye@gmail.com>
2012-07-17 11:03:24 +1200

    Add leaflet extensions to build script.

--- build/deps.js
+++ build/deps.js
@@ -1,36 +1,52 @@
 var deps = {
+	LeafletExtensions: {
+		src: [
+			'ext/LineUtil.js',
+			'ext/Polyline.js',
+			'ext/Polygon.js'
+		],
+		desc: 'Extensions to Leaflet to add intersection detection.'
+	},
+
 	DrawHandler: {
-		src: ['Handler.Draw.js'],
+		src: ['draw/Handler.Draw.js'],
 		desc: 'The base class for drawing.',
+		deps: ['LeafletExtensions']
 	},
 
 	Polys: {
-		src: ['Polyline.Draw.js',
-		      'Polygon.Draw.js'],
+		src: [
+			'draw/shapes/Polyline.Draw.js',
+			'draw/shapes/Polygon.Draw.js'
+		],
 		desc: 'Polyline and Polygon drawing handlers.',
 		deps: ['DrawHandler']
 	},
 	
 	SimpleShapes: {
-		src: ['SimpleShape.Draw.js',
-		      'Circle.Draw.js',
-		      'Rectangle.Draw.js'],
+		src: [
+			'draw/shapes/SimpleShape.Draw.js',
+			'draw/shapes/Circle.Draw.js',
+			'draw/shapes/Rectangle.Draw.js'
+		],
 		desc: 'Simple Shape drawing handlers.',
 		deps: ['DrawHandler']
 	},
 	
 	Markers: {
-		src: ['Marker.Draw.js'],
+		src: ['draw/shapes/Marker.Draw.js'],
 		desc: 'Marker drawing handlers.',
 		deps: ['DrawHandler']
 	},
 	
 	DrawingControl: {
-		src: ['Control.Draw.js'],
+		src: ['draw/Control.Draw.js'],
 		desc: 'Drawing control.',
-		deps: ['Polys',
-			   'SimpleShapes',
-			   'Markers']
+		deps: [
+			'Polys',
+			'SimpleShapes',
+			'Markers'
+		]
 	}
 };
 
