7ae69fe3cd7f774d53fbbd6b49eecf4716c29ebc
Hans Kristian Flaatten <hans@starefossen.com>
2012-12-06 15:12:09 +0100

    Some house cleaning
    Signed-off-by: Hans Kristian Flaatten <hans@starefossen.com>

--- src/ext/Polyline.Snapping.js
+++ src/ext/Polyline.Snapping.js
@@ -2,34 +2,48 @@ L.Polyline.include({
 	/**
 	 * Snap to function
 	 *
-	 * @param latlng {@code LatLng}
+	 * @param <LatLng> latlng -
+	 * @param <array> layers -
+	 * @param <numberic> sensitivity -
+	 *
+	 * @return <LatLng>
+	 *
+	 * @todo find the nearest point before returning
 	 */
 	snapTo: function (latlng, layers, sensitivity) {
 		var newLatLng = null;
 		var distance = 0;
-		
-		for (var i in layers[0]._layers) {
-			var map = layers[0]._map;
-			var obj = layers[0]._layers[i];
+
+		// Loop through layers
+		for (var l in layers) {
+			var map = layers[l]._map;
 			
-			var lastPoint = null;
-			for (var j in obj._latlngs) {
-				var ll = obj._latlngs[j];
-				var p1 = map.latLngToLayerPoint(latlng);
-				var p2 = map.latLngToLayerPoint(ll);
+			// Loop through features
+			for (var i in layers[0]._layers) {
+				var obj = layers[0]._layers[i];
 				
-				if (lastPoint != null) {
-					p3 = L.LineUtil.getClosestPoint(lastPoint, p2, p1, false);
+				// Loop through points
+				var lastPoint = null;
+				for (var j in obj._latlngs) {
+					var ll = obj._latlngs[j];
+					var p1 = map.latLngToLayerPoint(latlng);
+					var p2 = map.latLngToLayerPoint(ll);
 					
-					if (p1.distanceTo(p3) <= sensitivity) {
-						return map.layerPointToLatLng(p3);
+					if (lastPoint != null) {
+						p3 = L.LineUtil.getClosestPoint(lastPoint, p2, p1, false);
+						
+						if (p1.distanceTo(p3) <= sensitivity) {
+							return map.layerPointToLatLng(p3);
+						}
+					// This is kind of dirty
+					// But is needed in case a layer only contains 
+					// single points (markers)
+					} else if (p1.distanceTo(p2) <= sensitivity) {
+						return ll;
 					}
-				} else if (p1.distanceTo(p2) <= sensitivity) {
-					return ll;
+					
+					lastPoint = p2;
 				}
-				
-				
-				lastPoint = p2;
 			}
 		}
 		
