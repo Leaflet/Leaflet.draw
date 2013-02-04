0ee718b2aaae6638b44d6a63a5be0ab812a7ee55
Hans Kristian Flaatten <hans@starefossen.com>
2013-02-04 16:05:52 +0100

    Fixes #1 by @valeriomarchetti

--- src/ext/Polyline.Snapping.js
+++ src/ext/Polyline.Snapping.js
@@ -1,69 +1,78 @@
 L.Polyline.include({
-	/**
-	 * Temporarily snapping variables
-	 */
-	_snapVars: {
-		map : null,
-		minPoint : null,
-		minDist : null		
-	},
+  /**
+   * Temporarily snapping variables
+   */
+  _snapVars: {
+    map : null,
+    minPoint : null,
+    minDist : null    
+  },
 
-	/**
-	 * Snap to function
-	 *
-	 * @param <LatLng> latlng - cursor click
-	 *
-	 * @return <LatLng> - snapped to
-	 *
-	 * @todo find the closest point before returning?
-	 */
-	snapTo: function (latlng) {
-		var layers = this.options.snapping.layers;		
-		this._snapVars.minPoint = latlng;
-		this._snapVars.minDist = Infinity;
-		
-		// Loop through layers
-		for (var l1 in layers) {
-			for (var l2 in layers[l1]._layers) {
-				this._snapVars.map = layers[l1]._layers[l2]._map;
-				
-				if (typeof layers[l1]._layers[l2]._latlngs !== "undefined") {
-					this._snapToObject(latlng, layers[l1]._layers[l2]);
-				} else if (typeof layers[l1]._layers[l2]._layers !== "undefined") {
-					for (var l3 in layers[l1]._layers[l2]._layers) {
-						this._snapToObject(latlng, layers[l1]._layers[l2]._layers[l3]);
-					}
-				}	
-			}
-		}
-		
-		return this._snapVars.minPoint;
-	},
-	
-	_snapToObject: function (latlng, obj) {
-		var sensitivity = this.options.snapping.sensitivity
-			map = this._snapVars.map,
-			lastPoint = null;
-		
-		// Loop through points
-		for (var j in obj._latlngs) {
-			var ll = obj._latlngs[j],
-				p1 = map.latLngToLayerPoint(latlng),
-				p2 = map.latLngToLayerPoint(ll);
-			
-			if (lastPoint != null) {
-				p3 = L.LineUtil.getClosestPoint(lastPoint, p2, p1, false);
-				var tmpDist = p1.distanceTo(p3);
-				if (tmpDist <= sensitivity && tmpDist < this._snapVars.minDist) {
-					this._snapVars.minDist = tmpDist;
-					this._snapVars.minPoint = map.layerPointToLatLng(p3);
-				}
-			} else if (p1.distanceTo(p2) <= sensitivity && p1.distanceTo(p2) < this._snapVars.minDist) {
-				this._snapVars.minDist = p1.distanceTo(p2);
-				this._snapVars.minPoint = ll;
-			}
-			
-			lastPoint = p2;
-		}
-	}
+  /**
+   * Snap to function
+   *
+   * @param <LatLng> latlng - cursor click
+   *
+   * @return <LatLng> - snapped to
+   *
+   * @todo find the closest point before returning?
+   */
+  snapTo: function (latlng) {
+    var layers = this.options.snapping.layers;    
+    this._snapVars.minPoint = latlng;
+    this._snapVars.minDist = Infinity;
+    
+    // Loop through layers
+    for (var l1 in layers) {
+      for (var l2 in layers[l1]._layers) {
+        this._snapVars.map = layers[l1]._layers[l2]._map;
+        if (typeof layers[l1]._layers[l2]._latlngs !== "undefined") {
+          if (layers[l1]._layers[l2]._leaflet_id !== this._leaflet_id) {
+            this._snapToObject(latlng, layers[l1]._layers[l2]);
+          }
+        } else if (typeof layers[l1]._layers[l2]._layers !== "undefined") {
+          for (var l3 in layers[l1]._layers[l2]._layers) {
+            if (layers[l1]._layers[l2]._layers[l3]._leaflet_id !== this._leaflet_id) {
+              this._snapToObject(latlng, layers[l1]._layers[l2]._layers[l3]);
+            }
+          }
+        } 
+      }
+    }
+    
+    return this._snapVars.minPoint;
+  },
+  
+  /**
+   * Snap to object
+   *
+   * @param <LatLng> latlng - cursor click
+   * @param <FeatureLayer> object - layer to snap to
+  */
+  _snapToObject: function (latlng, obj) {
+    var sensitivity = this.options.snapping.sensitivity
+                map = this._snapVars.map,
+          lastPoint = null;
+    
+    // Loop through points
+    for (var j in obj._latlngs) {
+      var ll = obj._latlngs[j],
+        p1 = map.latLngToLayerPoint(latlng),
+        p2 = map.latLngToLayerPoint(ll);
+      
+      if (lastPoint != null) {
+        p3 = L.LineUtil.getClosestPoint(lastPoint, p2, p1, false);
+        var tmpDist = p1.distanceTo(p3);
+        if (tmpDist <= sensitivity && tmpDist < this._snapVars.minDist) {
+          this._snapVars.minDist = tmpDist;
+          this._snapVars.minPoint = map.layerPointToLatLng(p3);
+        }
+      } else if (p1.distanceTo(p2) <= sensitivity && p1.distanceTo(p2) < this._snapVars.minDist) {
+        this._snapVars.minDist = p1.distanceTo(p2);
+        this._snapVars.minPoint = ll;
+      }
+      
+      lastPoint = p2;
+    }
+  }
 });
