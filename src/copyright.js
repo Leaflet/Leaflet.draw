eca9054eadfce7b6e2b201e66a7d4c2066f8f5f6
jacobtoye <jacob.toye@gmail.com>
2013-02-11 10:33:02 +1300

    Moving copyright to it's own file.

---
+++ src/copyright.js
@@ -0,0 +1,8 @@
+/*
+	Leaflet.draw, a plugin that adds drawing and editing tools to Leaflet powered maps.
+	(c) 2012-2013, Jacob Toye, Smartrak
+
+	https://github.com/Leaflet/Leaflet.draw
+	http://leafletjs.com
+	https://github.com/jacobtoye
+*/
