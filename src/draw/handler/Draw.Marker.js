Leaflet/Leaflet.draw

commit 45a64bcccf7fb4d9df56f23e1a3fc2df5d3e5058

Fixes #80 when adding markers over polygons

src/draw/handler/Draw.Marker.js
@@ -20,7 +20,27 @@ L.Draw.Marker = L.Draw.Feature.extend({
 		
 		if (this._map) {
 			this._tooltip.updateContent({ text: 'Click map to place marker.' });
-			this._map.on('mousemove', this._onMouseMove, this);
+
+			// Same mouseMarker as in Draw.Polyline
+			if (!this._mouseMarker) {
+				this._mouseMarker = L.marker(this._map.getCenter(), {
+					icon: L.divIcon({
+						className: 'leaflet-mouse-marker',
+						iconAnchor: [20, 20],
+						iconSize: [40, 40]
+					}),
+					opacity: 0,
+					zIndexOffset: this.options.zIndexOffset
+				});
+			}
+
+			this._mouseMarker
+				.on('click', this._onClick, this)
+				.addTo(this._map);
+
+			this._map
+				.on('mousemove', this._onMouseMove, this)
+				.on('zoomend', this._onZoomEnd, this);
 		}
 	},
 
@@ -36,15 +56,22 @@ L.Draw.Marker = L.Draw.Feature.extend({
 				delete this._marker;
 			}
 
-			this._map.off('mousemove', this._onMouseMove);
-		}
+  		this._mouseMarker.off('click', this._onClick);
+  		this._map.removeLayer(this._mouseMarker);
+  		delete this._mouseMarker;
+  
+  		this._map
+  			.off('mousemove', this._onMouseMove)
+  			.off('zoomend', this._onZoomEnd);
+    }
 	},
 
 	_onMouseMove: function (e) {
 		var latlng = e.latlng;
 
 		this._tooltip.updatePosition(latlng);
-
+		this._mouseMarker.setLatLng(latlng);
+		
 		if (!this._marker) {
 			this._marker = new L.Marker(latlng, {
 				icon: this.options.icon,

commit b6587e1750f1bf465c2a682fb7d54b2557ddac21

Fixing issue introduced in 45a64bcccf7fb4d9df56f23e1a3fc2df5d3e5058. Fixes #85

src/draw/handler/Draw.Marker.js
@@ -38,9 +38,7 @@ L.Draw.Marker = L.Draw.Feature.extend({
 				.on('click', this._onClick, this)
 				.addTo(this._map);
 
-			this._map
-				.on('mousemove', this._onMouseMove, this)
-				.on('zoomend', this._onZoomEnd, this);
+			this._map.on('mousemove', this._onMouseMove, this);
 		}
 	},
 
@@ -60,9 +58,7 @@ L.Draw.Marker = L.Draw.Feature.extend({
 			this._map.removeLayer(this._mouseMarker);
 			delete this._mouseMarker;
 
-			this._map
-				.off('mousemove', this._onMouseMove)
-				.off('zoomend', this._onZoomEnd);
+			this._map.off('mousemove', this._onMouseMove);
 		}
 	},
 