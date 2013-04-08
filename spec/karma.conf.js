ddfa1c801720338ed9bfa90e5b286e275ed50a40
iirvine <icirvine@gmail.com>
2013-04-08 15:16:57 -0700

    put karma.conf back into CI mode

--- spec/karma.conf.js
+++ spec/karma.conf.js
@@ -49,7 +49,7 @@ colors = true;
 logLevel = LOG_WARN;
 
 // enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
+autoWatch = false;
 
 // Start these browsers, currently available:
 // - Chrome
@@ -66,4 +66,4 @@ captureTimeout = 5000;
 
 // Continuous Integration mode
 // if true, it capture browsers, run tests and exit
-// singleRun = true;
+singleRun = true;
