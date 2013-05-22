Leaflet/Leaflet.draw

commit d1347433fde0b25290c3c287ab743faeba6b0596

Merge the options in DrawToolbar to ensure the titles are copied over.

src/draw/DrawToolbar.js
@@ -19,6 +19,11 @@ L.DrawToolbar = L.Toolbar.extend({
 	},
 
 	initialize: function (options) {
+		// Ensure that the options are merged correctly since L.extend is only shallow
+		for(var type in this.options) {
+			options[type] = L.extend({}, this.options[type], options[type]);
+		}
+
 		L.Toolbar.prototype.initialize.call(this, options);
 	},
 

commit b565b3d29da3e97d9261b5374cf51f8f198e1c84

Allow to disable handlers (broken in d1347433fde0b25290c3c287ab743faeba6b0596)

src/draw/DrawToolbar.js
@@ -22,7 +22,9 @@ L.DrawToolbar = L.Toolbar.extend({
 		// Ensure that the options are merged correctly since L.extend is only shallow
 		for (var type in this.options) {
 			if (this.options.hasOwnProperty(type)) {
-				options[type] = L.extend({}, this.options[type], options[type]);
+				if (options[type]) {
+					options[type] = L.extend({}, this.options[type], options[type]);
+				}
 			}
 		}
 