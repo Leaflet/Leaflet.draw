b14fa762c74a79a29c689bcd3f037bf9d45dc665
jacobtoye <jacob.toye@gmail.com>
2013-06-19 11:07:56 +1200

    jshinting

--- src/ext/PolygonUtil.js
+++ src/ext/PolygonUtil.js
@@ -4,14 +4,14 @@
 
 L.PolygonUtil = {
 	// Ported from the OpenLayers implementation. See https://github.com/openlayers/openlayers/blob/master/lib/OpenLayers/Geometry/LinearRing.js#L270
-	geodesicArea: function(latLngs) {
+	geodesicArea: function (latLngs) {
 		var pointsCount = latLngs.length,
 			area = 0.0,
 			d2r = L.LatLng.DEG_TO_RAD,
 			p1, p2;
 
-		if(pointsCount > 2) {
-			for(var i = 0; i < pointsCount; i++) {
+		if (pointsCount > 2) {
+			for (var i = 0; i < pointsCount; i++) {
 				p1 = latLngs[i];
 				p2 = latLngs[(i + 1) % pointsCount];
 				area += ((p2.lng - p1.lng) * d2r) *
