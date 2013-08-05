6eb6f1a8aefcf0c010074597c0ae6bbf94b000cb
jacobtoye <jacob.toye@gmail.com>
2013-08-06 09:12:31 +1200

    Fixing issue that broke polygon creation when showArea = flase or allowIntersection = true.

--- src/draw/handler/Draw.Polygon.js
+++ src/draw/handler/Draw.Polygon.js
@@ -66,6 +66,10 @@ L.Draw.Polygon = L.Draw.Polyline.extend({
 		var area = this._area,
 			areaStr;
 
+		if (!area) {
+			return null;
+		}
+
 		if (this.options.metric) {
 			if (area >= 10000) {
 				areaStr = (area * 0.0001).toFixed(2) + ' ha';
