c184e70a4708046adc3c4d1b4b691131b1961ead
Tom MacWright <tom@macwright.org>
2013-09-17 18:19:51 -0400

    Expand specs, adding tests for utils

---
+++ spec/suites/GeometryUtilSpec.js
@@ -0,0 +1,25 @@
+describe("L.GeometryUtil", function(){
+	var map, control, container;
+
+	it("geodesicArea", function() {
+		expect(L.GeometryUtil.geodesicArea([
+            { lat: 0,  lng: 0 },
+            { lat: 0,  lng: 10 },
+            { lat: 10, lng: 10 },
+            { lat: 10, lng: 0 },
+            { lat: 0,  lng: 0 }
+        ])).to.eql(1232921098571.292);
+	});
+
+	describe("readableDistance", function() {
+        it("metric", function() {
+            expect(L.GeometryUtil.readableDistance(1000, true)).to.eql('1000 m');
+            expect(L.GeometryUtil.readableDistance(1500, true)).to.eql('1.50 km');
+        });
+
+        it("imperial", function() {
+            expect(L.GeometryUtil.readableDistance(1609.3488537961)).to.eql('1760 yd');
+            expect(L.GeometryUtil.readableDistance(1610.3488537961)).to.eql('1.00 miles');
+        });
+    });
+});
---
+++ spec/suites/LatLngUtilSpec.js
@@ -0,0 +1,9 @@
+describe("L.LatLngUtil", function(){
+	it("cloneLatLngs", function() {
+        var latLngs = [{ lat: 0, lng: 0 }];
+        var clone = L.LatLngUtil.cloneLatLngs(latLngs);
+        expect(clone[0].lat).to.eql(latLngs[0].lat);
+        clone[0].lat = 10;
+        expect(latLngs[0].lat).to.eql(0);
+	});
+});
