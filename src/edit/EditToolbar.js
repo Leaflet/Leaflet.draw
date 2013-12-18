61c9841c095ac115dbaf15c5d68497de61201f92
Manuel Baclet <manuel@edagames.com>
2013-12-18 17:17:33 +0100

    When in edit/delete mode, revertLayers has to be called when another handler is clicked on.

--- src/edit/EditToolbar.js
+++ src/edit/EditToolbar.js
@@ -107,7 +107,12 @@ L.EditToolbar = L.Toolbar.extend({
 
 		this.options.featureGroup.off('layeradd layerremove', this._checkDisabled, this);
 	},
-
+	_handlerActivated: function (e) {
+		if (this._activeMode && this._activeMode.handler.enabled()) {
+			this._activeMode.handler.revertLayers();
+		}
+		L.Toolbar.prototype._handlerActivated.call(this, e);
+	},
 	disable: function () {
 		if (!this.enabled()) { return; }
 
