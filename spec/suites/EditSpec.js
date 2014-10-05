b9b3a784ac82007388a78ecbfebdd9b3f8b5c15a
Justin Manley <manleyjster@gmail.com>
2014-10-05 14:13:46 -0500

    Add two tests to ensure that styles are set and reverted correctly when editing mode is enabled and disabled.

--- spec/suites/EditSpec.js
+++ spec/suites/EditSpec.js
@@ -33,4 +33,39 @@ describe("L.Edit", function () {
 			expect(circle.getLatLng()).to.eql(newLatLng);
 		});
 	});
+
+	describe("L.Edit.Poly", function () {
+		var edit,
+			drawnItems,
+			poly;
+
+		beforeEach(function () {
+			drawnItems = new L.FeatureGroup().addTo(map);
+			edit = new L.EditToolbar.Edit(map, {
+				featureGroup: drawnItems,
+				selectedPathOptions: L.EditToolbar.prototype.options.edit.selectedPathOptions
+			});
+			poly = new L.Polyline(L.latLng(41, -87), L.latLng(42, -88));
+		});
+
+		it("Should change the style of the polyline during editing mode.", function () {
+			var originalOptions = L.extend({}, poly.options);
+
+			drawnItems.addLayer(poly);
+			edit.enable();
+
+			expect(poly.editing.enabled()).to.equal(true);
+			expect(poly.options).not.to.eql(originalOptions);
+		});
+
+		it("Should revert to original styles when editing is toggled.", function () {
+			var originalOptions = L.extend({maintainColor: false }, poly.options);
+
+			drawnItems.addLayer(poly);
+			edit.enable();
+			edit.disable();
+
+			expect(poly.options).to.eql(originalOptions);
+		});
+	});
 });
