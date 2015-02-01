1f1670d6c95798ba49680299e3ccf106b099a043
Justin Manley <manleyjster@gmail.com>
2015-01-31 21:22:56 -0600

    Add drawControl and drawControlTooltips options to L.Map. Fixes Leaflet.toolbar#17 and ensures that helpful tooltips are shown by default when using Leaflet.draw.

--- src/draw/DrawToolbar.js
+++ src/draw/DrawToolbar.js
@@ -1,1 +1,16 @@
 L.DrawToolbar = {};
+
+/*
+ * Defaults added to map for convenience.
+ */
+L.Map.mergeOptions({
+    drawControlTooltips: true,
+    drawControl: false
+});
+
+L.Map.addInitHook(function () {
+	if (this.options.drawControl) {
+		this.drawControl = new L.DrawToolbar.Control();
+		this.addLayer(this.drawControl);
+	}
+});
