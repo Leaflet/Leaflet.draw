98229854d7d0d02d648e281fa9d140d3c6fc149f
Justin Manley <manleyjster@gmail.com>
2015-02-08 21:23:43 -0600

    Tweak EditToolbar.Popup to offset the popup toolbar if it is opened on a marker (fixes #18 and #9).

--- src/edit/popup/EditToolbar.Popup.js
+++ src/edit/popup/EditToolbar.Popup.js
@@ -4,5 +4,16 @@ L.EditToolbar.Popup = L.Toolbar.Popup.extend({
 			L.Edit.Popup.Edit,
 			L.Edit.Popup.Delete
 		]
+	},
+
+	onAdd: function (map) {
+		var shape = this._arguments[1];
+
+		if (shape instanceof L.Marker) {
+			/* Adjust the toolbar position so that it doesn't cover the marker. */
+			this.options.anchor = L.point(shape.options.icon.options.popupAnchor);
+		}
+
+		L.Toolbar.Popup.prototype.onAdd.call(this, map);
 	}
 });
