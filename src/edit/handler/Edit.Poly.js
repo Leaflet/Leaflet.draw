db7614b44912cda4ca7ecd31995614172d54ce8d
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2015-09-15 21:36:02 +0200

    Debug editing PolyLines - no poly.options.editing.fill property

--- src/edit/handler/Edit.Poly.js
+++ src/edit/handler/Edit.Poly.js
@@ -21,7 +21,8 @@ L.Edit.Poly = L.Handler.extend({
 		var poly = this._poly;
 
 		if (!(poly instanceof L.Polygon)) {
-			poly.options.editing.fill = false;
+                        if(poly.options.editing)
+				poly.options.editing.fill = false;
 		}
 
 		poly.setStyle(poly.options.editing);
