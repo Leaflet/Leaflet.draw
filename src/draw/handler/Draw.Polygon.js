81ae6532f6f4b61654d18ab7249a8e968480cbb2
xwp1990123 <1542453460@qq.com>
2016-07-02 10:52:01 +0800

    add polygon.options.metrics to show metric area
    fix showArea functions in metrics during drawing a polygon by adding a metrics options

--- src/draw/handler/Draw.Polygon.js
+++ src/draw/handler/Draw.Polygon.js
@@ -16,7 +16,8 @@ L.Draw.Polygon = L.Draw.Polyline.extend({
 			fillColor: null, //same as color by default
 			fillOpacity: 0.2,
 			clickable: true
-		}
+		},
+		metric: true // Whether to use the metric measurement system or imperial
 	},
 
 	initialize: function (map, options) {
