783f82d4e0eb5b82116e5635e846b7facb155d99
Jon West <jowest@corelogic.com>
2016-11-09 22:54:09 -0600

    v0.4.3

--- dist/leaflet.draw-src.js
+++ dist/leaflet.draw-src.js
@@ -1,5 +1,5 @@
 /*
- Leaflet.draw 0.4.3+e21f99b, a plugin that adds drawing and editing tools to Leaflet powered maps.
+ Leaflet.draw 0.4.3, a plugin that adds drawing and editing tools to Leaflet powered maps.
  (c) 2012-2017, Jacob Toye, Jon West, Smartrak, Leaflet
 
  https://github.com/Leaflet/Leaflet.draw
@@ -8,7 +8,7 @@
 (function (window, document, undefined) {/**
  * Leaflet.draw assumes that you have already included the Leaflet library.
  */
-L.drawVersion = "0.4.3+e21f99b";
+L.drawVersion = "0.4.3";
 /**
  * @class L.Draw
  * @aka Draw
