2d661db04f1e041d34884bbd06f2f4387c9e0422
Jon West <jowest@corelogic.com>
2016-11-29 18:04:41 -0600

    Geometry now have nautic display options

--- src/draw/handler/Draw.Polyline.js
+++ src/draw/handler/Draw.Polyline.js
@@ -37,6 +37,7 @@ L.Draw.Polyline = L.Draw.Feature.extend({
 		},
 		metric: true, // Whether to use the metric measurement system or imperial
 		feet: true, // When not metric, to use feet instead of yards for display.
+		nautic: false, // When not metric, not feet use nautic mile for display
 		showLength: true, // Whether to display distance in the tooltip
 		zIndexOffset: 2000 // This should be > than the highest z-index any map layers
 	},
@@ -458,7 +459,7 @@ L.Draw.Polyline = L.Draw.Feature.extend({
 		// calculate the distance from the last fixed point to the mouse position
 		distance = this._measurementRunningTotal + currentLatLng.distanceTo(previousLatLng);
 
-		return L.GeometryUtil.readableDistance(distance, this.options.metric, this.options.feet);
+		return L.GeometryUtil.readableDistance(distance, this.options.metric, this.options.feet, this.options.nautic);
 	},
 
 	_showErrorTooltip: function () {
