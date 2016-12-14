d85bf9031fc375c80debcf57cbf949e6ef442b0d
Thomas Grice <thomas.grice@micasense.com>
2016-12-13 21:19:39 -0800

    remove logs

--- src/draw/handler/Draw.Polyline.js
+++ src/draw/handler/Draw.Polyline.js
@@ -483,11 +483,9 @@ L.Draw.Polyline = L.Draw.Feature.extend({
 	},
 
 	_getMeasurementString: function () {
-		window.console.log('entering fn');
 		var currentLatLng = this._currentLatLng,
 			previousLatLng = this._markers[this._markers.length - 1].getLatLng(),
 			distance;
-		window.console.log('were in getmeasurementstring ', currentLatLng, previousLatLng, distance);
 		// calculate the distance from the last fixed point to the mouse position
 		distance = this._measurementRunningTotal + currentLatLng.distanceTo(previousLatLng);
 
