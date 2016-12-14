0cce84af45bcd9f67eb22ce9f8a6979e0dca1f14
Thomas Grice <thomas.grice@micasense.com>
2016-12-13 23:53:19 -0800

    forgot build files

--- dist/leaflet.draw-src.js
+++ dist/leaflet.draw-src.js
@@ -1,5 +1,5 @@
 /*
- Leaflet.draw 0.4.7+daa1a54, a plugin that adds drawing and editing tools to Leaflet powered maps.
+ Leaflet.draw 0.4.7+9ba5785, a plugin that adds drawing and editing tools to Leaflet powered maps.
  (c) 2012-2017, Jacob Toye, Jon West, Smartrak, Leaflet
 
  https://github.com/Leaflet/Leaflet.draw
@@ -8,7 +8,7 @@
 (function (window, document, undefined) {/**
  * Leaflet.draw assumes that you have already included the Leaflet library.
  */
-L.drawVersion = "0.4.7+daa1a54";
+L.drawVersion = "0.4.7+9ba5785";
 /**
  * @class L.Draw
  * @aka Draw
@@ -749,7 +749,7 @@ L.Draw.Polyline = L.Draw.Feature.extend({
 			var lastPtDistance = Infinity;
 			if (this._markers.length > 0) {
 				var lastMarkerPoint = this._map.latLngToContainerPoint(this._markers[this._markers.length - 1].getLatLng());
-				lastPtDistance = L.point(clientX, clientY).distanceTo(lastMarkerPoint);
+				lastPtDistance = this._mouseDownOrigin.distanceTo(lastMarkerPoint);
 			}
 			window.console.log('da lastptdistance ', lastPtDistance);
 			if (lastPtDistance < 60 && L.Browser.touch) {
