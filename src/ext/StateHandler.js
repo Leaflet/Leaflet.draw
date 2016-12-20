3bec394e4b256558e21c5f4e4adddb2711c903a9
Joseph Ryan <jfr7p@virginia.edu>
2016-12-20 14:37:08 -0800

    Priority Queue for Undo's StateHandler
    Priority Queue for Undo's StateHandler to prevent key-triggered race
    condition that could sometimes occur if the user smashed ctrlZ/ctrlY
    very quickly.

--- src/ext/StateHandler.js
+++ src/ext/StateHandler.js
@@ -20,7 +20,7 @@ L.Draw.StateHandler =  L.Class.extend({
         return this._enabled;
     },
     
-    enable : function () {
+    enable: function () {
         if (this._enabled) {
             return;
         }
@@ -28,10 +28,11 @@ L.Draw.StateHandler =  L.Class.extend({
         this._enabled = true;
         this._idCounter = 0;
     
+        this.stateLock = false;
+        this.stateTodo = [];
+        
         this.undoStack = [];
-        this.undoIndex = {};
         this.redoStack = [];
-        this.redoIndex = {};
         
         if (this.undoAction != null) {
             this._map.on('undoaction', this.undoAction, this);
@@ -51,7 +52,7 @@ L.Draw.StateHandler =  L.Class.extend({
         }
     },
     
-    disable : function () {
+    disable: function () {
         if (! this._enabled) {
             return;
         }
@@ -74,9 +75,9 @@ L.Draw.StateHandler =  L.Class.extend({
         }
     },
     
-    bindKey : function (key, action) {
+    bindKey: function (key, action) {
         var checkCtl = (key.indexOf('ctrl') > -1);
-        var checkedKey = (checkCtl) ? key.substr(-1) : key;
+        var checkedKey = (checkCtl) ? key.substr(-1): key;
         checkedKey = checkedKey.toLowerCase();
 
         this.keyChecker[key] = function (e) {
@@ -91,32 +92,32 @@ L.Draw.StateHandler =  L.Class.extend({
         L.DomEvent.on(document, 'keyup', this.keyChecker[key], this);
     },
     
-    unbindKey : function (key) {
+    unbindKey: function (key) {
         L.DomEvent.off(document, 'keyup', this.keyChecker[key], this);
         delete this.keyChecker[key];
     },
 
-    clear : function () {
+    clear: function () {
         this.undoStack = new Array();
         this.redoStack = new Array();
     },
     
-    discardLastPush : function () {
+    discardLastPush: function () {
         this.undoStack.pop();
     },
         
-    clearNested : function (numTimes) {
+    clearNested: function (numTimes) {
         this.redoStack = new Array();
         for (var i=0; i<numTimes; i++) {
             this.undoStack.pop();
         }
     },
         
-    pushUndo : function (actionType, params) {
+    pushUndo: function (actionType, params) {
         this.undoStack.push({
-            'actionType' : actionType,
-            'params' : params,
-            'undoId' : this._idCounter
+            'actionType': actionType,
+            'params': params,
+            'undoId': this._idCounter
         });
         
         this._idCounter ++;
@@ -126,24 +127,65 @@ L.Draw.StateHandler =  L.Class.extend({
         }
     },
         
-    undo : function () {
-        if (this.undoStack.length > 0) {
-            var lastAction = this.undoStack.pop();
-            this.redoStack.push(lastAction);
-            this._map.fire('undoaction', lastAction);
+    undo: function () {
+        this.stateTodo.push('undo');
+        
+        if (this.stateLock) {
+            return;
         }
+        
+        this.stateLock = true;
+        this.processState();
     },
     
-    putbackLastUndo : function () {
+    putbackLastUndo: function () {
         var lastAction = this.redoStack.pop();
         this.undoStack.push(lastAction);
     },
     
-    redo : function () {
-        if (this.redoStack.length > 0) {
-            var lastAction = this.redoStack.pop();
-            this.undoStack.push(lastAction);
-            this._map.fire('redoaction', lastAction);
+    redo: function () {
+        this.stateTodo.push('redo');
+        
+        if (this.stateLock) {
+            return;
         }
+        
+        this.stateLock = true;
+        this.processState();
+    },
+    
+    // in the original version of this class, it was possible to trigger some sort of deep recursion error if ctrlZ/ctrlY were smashed very quickly.
+    // my best conclusion as to why this occured was that some browsers seem to detect keyboard events in a seperate thread, and thus a race condition
+    // could potentially develop from fast key input. thus, instead of executing the undo/redo action directly from the request, we instead add the
+    // triggered undo/redos into a priority queue in order to ensure we only ever process one at a time.
+    processState: function () {
+        if (this.stateTodo.length == 0) {
+            // only set false at the base case, as setTimeout actually returns immediately
+            this.stateLock = false;
+            return;
+        }
+        var nextAction = this.stateTodo.shift();
+    
+        if (nextAction == 'redo') {
+            if (this.redoStack.length > 0) {
+                var lastAction = this.redoStack.pop();
+                this.undoStack.push(lastAction);
+                this._map.fire('redoaction', lastAction);
+            }
+        }
+        else {
+            if (this.undoStack.length > 0) {
+                var lastAction = this.undoStack.pop();
+                this.redoStack.push(lastAction);
+                this._map.fire('undoaction', lastAction);
+            }
+        }
+        
+        // add a small amount of waiting time, 100ms should be fine - the error I saw when smashing ctrlZ/ctrlY very
+        // quickly might actually be related to the display lagging behind the processing... it's hard to determine.
+        // note that L.Draw uses setTimeout internally to handle error processing.
+        // anyways, the user shouldn't notice 100ms of delay, so why not be safe than sorry?
+        var that = this;
+        setTimeout(function () { that.processState() }, 100);
     },
 });
