e9fe75a488ac8173695e2ba9fd5618cc3e1577bb
vegter <amsjurien@gmail.com>
2017-01-17 13:52:23 +0100

    Locale number formatting added

--- spec/suites/GeometryUtilSpec.js
+++ spec/suites/GeometryUtilSpec.js
@@ -34,4 +34,90 @@ describe("L.GeometryUtil", function () {
 			expect(L.GeometryUtil.readableDistance(1610.3488537961, 'nauticalMile')).to.eql('0.87 nm');
 		});
 	});
+
+	describe("formatted number", function () {
+		it("accepts a thousands seperator", function () {
+            L.drawLocal.format = {
+                numeric: {
+                    delimiters: {
+                        thousands: '#',
+                    }
+                }
+            };
+            expect(L.GeometryUtil.formattedNumber(100)).to.eql('100');
+            expect(L.GeometryUtil.formattedNumber(1000)).to.eql('1#000');
+            expect(L.GeometryUtil.formattedNumber(1000000)).to.eql('1#000#000');
+            expect(L.GeometryUtil.formattedNumber(100, 0)).to.eql('100');
+            expect(L.GeometryUtil.formattedNumber(1000, 0)).to.eql('1#000');
+            expect(L.GeometryUtil.formattedNumber(1000000, 0)).to.eql('1#000#000');
+            expect(L.GeometryUtil.formattedNumber(100, 2)).to.eql('100.00');
+            expect(L.GeometryUtil.formattedNumber(1000, 2)).to.eql('1#000.00');
+            expect(L.GeometryUtil.formattedNumber(1000000, 2)).to.eql('1#000#000.00');
+		});
+
+        it("accepts a decimal seperator", function () {
+            L.drawLocal.format = {
+                numeric: {
+                    delimiters: {
+                        decimal: '$'
+                    }
+                }
+            };
+            expect(L.GeometryUtil.formattedNumber(100)).to.eql('100');
+            expect(L.GeometryUtil.formattedNumber(1000)).to.eql('1000');
+            expect(L.GeometryUtil.formattedNumber(1000000)).to.eql('1000000');
+            expect(L.GeometryUtil.formattedNumber(100, 0)).to.eql('100');
+            expect(L.GeometryUtil.formattedNumber(1000, 0)).to.eql('1000');
+            expect(L.GeometryUtil.formattedNumber(1000000, 0)).to.eql('1000000');
+            expect(L.GeometryUtil.formattedNumber(100, 2)).to.eql('100$00');
+            expect(L.GeometryUtil.formattedNumber(1000, 2)).to.eql('1000$00');
+            expect(L.GeometryUtil.formattedNumber(1000000, 2)).to.eql('1000000$00');
+        });
+
+        it("accepts a thousands and a decimal seperator", function () {
+            L.drawLocal.format = {
+                numeric: {
+                    delimiters: {
+                        thousands: '#',
+                        decimal: '$'
+                    }
+                }
+            };
+            expect(L.GeometryUtil.formattedNumber(100)).to.eql('100');
+            expect(L.GeometryUtil.formattedNumber(1000)).to.eql('1#000');
+            expect(L.GeometryUtil.formattedNumber(1000000)).to.eql('1#000#000');
+            expect(L.GeometryUtil.formattedNumber(100, 0)).to.eql('100');
+            expect(L.GeometryUtil.formattedNumber(1000, 0)).to.eql('1#000');
+            expect(L.GeometryUtil.formattedNumber(1000000, 0)).to.eql('1#000#000');
+            expect(L.GeometryUtil.formattedNumber(100, 2)).to.eql('100$00');
+            expect(L.GeometryUtil.formattedNumber(1000, 2)).to.eql('1#000$00');
+            expect(L.GeometryUtil.formattedNumber(1000000, 2)).to.eql('1#000#000$00');
+        });
+
+        it("defaults to no thousands and decimal dot", function () {
+            delete L.drawLocal.format;
+            expect(L.GeometryUtil.formattedNumber(100)).to.eql('100');
+            expect(L.GeometryUtil.formattedNumber(1000)).to.eql('1000');
+            expect(L.GeometryUtil.formattedNumber(1000000)).to.eql('1000000');
+            expect(L.GeometryUtil.formattedNumber(100, 0)).to.eql('100');
+            expect(L.GeometryUtil.formattedNumber(1000, 0)).to.eql('1000');
+            expect(L.GeometryUtil.formattedNumber(1000000, 0)).to.eql('1000000');
+            expect(L.GeometryUtil.formattedNumber(100, 2)).to.eql('100.00');
+            expect(L.GeometryUtil.formattedNumber(1000, 2)).to.eql('1000.00');
+            expect(L.GeometryUtil.formattedNumber(1000000, 2)).to.eql('1000000.00');
+        });
+
+        it("is used for readableDistance and readableArea", function () {
+			L.drawLocal.format = {
+				numeric: {
+					delimiters: {
+						thousands: '.',
+						decimal: ','
+					}
+				}
+			};
+			expect(L.GeometryUtil.readableDistance(1000, true)).to.eql('1.000 m');
+            expect(L.GeometryUtil.readableArea(50000, true)).to.eql('5,00 ha');
+		});
+	});
 });
