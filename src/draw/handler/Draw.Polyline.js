088025615f8aae95580d3a33014339f09329b898
Bobby <Slobodan.Simic@digitalglobe.com>
2017-08-02 13:37:12 -0400

    Add max marker to polyline.

--- src/draw/handler/Draw.Polyline.js
+++ src/draw/handler/Draw.Polyline.js
@@ -39,8 +39,9 @@ L.Draw.Polyline = L.Draw.Feature.extend({
 		feet: true, // When not metric, to use feet instead of yards for display.
 		nautic: false, // When not metric, not feet use nautic mile for display
 		showLength: true, // Whether to display distance in the tooltip
-    zIndexOffset: 2000, // This should be > than the highest z-index any map layers
-    factor: 1 // To change distance calculation
+        zIndexOffset: 2000, // This should be > than the highest z-index any map layers
+        factor: 1, // To change distance calculation
+        maxPoints: 0 // Once this number of points are placed, finish shape
 	},
 
 	// @method initialize(): void
@@ -298,7 +299,10 @@ L.Draw.Polyline = L.Draw.Feature.extend({
 			var dragCheckDistance = L.point(clientX, clientY)
 				.distanceTo(this._mouseDownOrigin);
 			var lastPtDistance = this._calculateFinishDistance(e.latlng);
-			if (lastPtDistance < 10 && L.Browser.touch) {
+            if(this.options.maxPoints > 1 && this.options.maxPoints == this._markers.length+1) {
+                this.addVertex(e.latlng);
+                this._finishShape();
+            } else if (lastPtDistance < 10 && L.Browser.touch) {
 				this._finishShape();
 			} else if (Math.abs(dragCheckDistance) < 9 * (window.devicePixelRatio || 1)) {
 				this.addVertex(e.latlng);
